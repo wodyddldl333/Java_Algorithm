f698a0800140a4cc44fec977cc57700cbcdcfa8b
Jaeyong <158774926+wodyddldl333@users.noreply.github.com>
2024-05-07 16:22:16 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 540 ms, Memory: 61860 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,85 +1,103 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static int K, N, M, arr[][];
-	private static boolean[][][] visit;
-	private static int[][] deltas = {{1,0}, {0,1}, {-1, 0}, {0, -1}};
-	private static int[][] superjump = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
-	
-	private static class Monkey {
-		int x;
-		int y;
-		int time;
-		int hores;
-		
-		public Monkey(int x, int y, int time, int hores) {
-			this.x = x;
-			this.y = y;
-			this.time = time;
-			this.hores = hores;
-		}
-		
-	}
-	public static void main(String[] args) throws Exception {
+
+	static int W,H,K,ans;
+	static int[][] map;
+	static int[][] move = {{-1,0},{1,0},{0,1},{0,-1}};
+	static int[][] horse = {{2,1},{-2,1},{2,-1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
+	static boolean[][][] visited;
+
+	public static void main(String[] args)throws Exception{
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		K = Integer.parseInt(in.readLine());
+
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		K = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[M][N];
-		visit = new boolean[M][N][K + 1];
-		
-		for (int i = 0; i < M; i++) {
+
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+
+		map = new int[H][W];
+
+		for(int i=0;i<H;i++) {
 			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			for(int j=0;j<W;j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		int result = -1;
-		
-		Queue<Monkey> q = new LinkedList<>();
-		visit[0][0][0] = true;
-		
-		q.add(new Monkey(0, 0, 0, 0));
-		
-		while (!q.isEmpty()) {
-			Monkey curr = q.poll();
-			
-			if (curr.x == M - 1 && curr.y == N - 1) {
-				result = curr.time;
+
+		// TODO: 1. 일단 3차원 배열에서 작은 수가 앞으로 오면 메모리 절약
+		visited = new boolean[H][W][K+1];
+
+		ans = -1;
+
+		bfs(0, 0);
+
+		System.out.println(ans);
+
+	}
+
+	static void bfs(int starti,int startj) {
+
+		Queue<int[]> q = new ArrayDeque<>();
+		// TODO: 2. 클래스로 하는게 오히려 안헷갈릴 수 있을 듯
+		q.offer(new int[] {starti, startj, 0, 0});
+		visited[starti][startj][0]=true;
+
+
+		while(!q.isEmpty()) {
+			int[] tmp = q.poll();
+			int nowi = tmp[0];
+			int nowj = tmp[1];
+			int k = tmp[2];
+			int cnt = tmp[3];
+
+			if(nowi==H-1 && nowj==W-1) {
+				ans = cnt;
 				break;
 			}
-			
-			for (int[] delta : deltas) {
-				int nx = curr.x + delta[0];
-				int ny = curr.y + delta[1];
-				
-				if (check(nx, ny) && !visit[nx][ny][curr.hores]) {
-					visit[nx][ny][curr.hores] = true; 
-					q.add(new Monkey(nx, ny, curr.time + 1, curr.hores));
+
+			for(int i=0;i<4;i++) {
+
+				int nexti = nowi +move[i][0];
+				int nextj = nowj +move[i][1];
+
+				if(!isIn(nexti,nextj)) continue;
+				if(map[nexti][nextj]==1) continue;
+
+				if(!visited[nexti][nextj][k]) {
+
+					visited[nexti][nextj][k]=true;
+					q.offer(new int[] {nexti,nextj,k,cnt+1});
 				}
 			}
-			
-			if (curr.hores >= K) continue;
-			
-			for (int[] delta : superjump) {
-				int nx = curr.x + delta[0];
-				int ny = curr.y + delta[1];
-				
-				if (check(nx, ny) && !visit[nx][ny][curr.hores + 1]) {
-					visit[nx][ny][curr.hores + 1] = true; 
-					q.add(new Monkey(nx, ny, curr.time + 1, curr.hores + 1));
+
+			if(k < K) {
+
+				for(int i=0;i<8;i++) {
+					int nexti = nowi +horse[i][0];
+					int nextj = nowj +horse[i][1];
+
+					if(!isIn(nexti,nextj)) continue;
+					if(map[nexti][nextj]==1) continue;
+
+					if(!visited[nexti][nextj][k+1]) {
+						visited[nexti][nextj][k+1]=true;
+						q.offer(new int[] {nexti,nextj,k+1,cnt+1});
+					}
 				}
 			}
 		}
-		
-		System.out.println(result);
-		
+
 	}
-	private static boolean check(int nx, int ny) {
-		return nx >= 0 && nx < M && ny >= 0 && ny < N && arr[nx][ny] == 0;
+
+	static boolean isIn(int a,int b) {
+		return a>=0 && b>=0 && a<H && b<W;
 	}
 }
